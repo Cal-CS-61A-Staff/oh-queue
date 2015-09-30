eb5ddc40e285f683d49f7a46a838ce3514bc39cc
Jordon Wing <jordonwii@gmail.com>
2015-09-29 20:46:54 -0700

    Move 'current date' logic into the server-side.

--- oh_queue/views.py
+++ oh_queue/views.py
@@ -1,3 +1,5 @@
+import datetime
+
 from flask import render_template
 
 from oh_queue import app
@@ -8,10 +10,10 @@ from oh_queue.auth import requires_auth, requires_admin
 @app.route("/")
 def index():
     entries = Entry.query.filter_by(status=ENTRY.PENDING).order_by(Entry.add_date).all()
-    return render_template('main.html', entries=entries)
+    return render_template('main.html', entries=entries, date=datetime.datetime.now())
 
 
 @app.route("/assist")
 def assist():
     entries = Entry.query.filter_by(status=ENTRY.PENDING).order_by(Entry.add_date).all()
-    return render_template('assist.html', entries=entries)
+    return render_template('assist.html', entries=entries, date=datetime.datetime.now())
