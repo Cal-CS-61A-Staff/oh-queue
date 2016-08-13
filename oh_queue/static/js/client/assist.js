885473c79ca70e142c305b927ab4b57d98e4115e
Kyle Raftogianis <knrafto@gmail.com>
2016-08-13 15:17:05 -0700

    Use event delegates

--- oh_queue/static/js/client/assist.js
+++ oh_queue/static/js/client/assist.js
@@ -1,20 +1,13 @@
 $(document).ready(function(){
-
-  // helper functions
-  var resolveHandler = function (event) {
-     var url = $(this).attr('data-url');
-     var postData = {
-      id: $(this).attr('data-id')
-     };
-     $.post(url, postData, function(data) {
-          //nothing needs to be done here
-      });
-
-  };
+  requestNotificationPermission();
 
   // Bind event listeners
-  $('.resolve').click(resolveHandler);
-  requestNotificationPermission();
+  $('body').on('click', '.resolve', function(event) {
+     var url = $(this).attr('data-url');
+     $.post(url, {
+       id: $(this).attr('data-id')
+     });
+  });
 
   // opening Socket
   var socket = io.connect(
@@ -24,14 +17,13 @@ $(document).ready(function(){
   // Socket handler for adding entries
   socket.on('add_entry_response', function(message) {
       $('#queue').append(message.html);
-      $('#resolve-' + message.id).click(resolveHandler);
       var details = {
           body: message.name + " - " + message.assignment + message.question + " in " + message.location
       }
       notifyUser("OH Queue: " + message.name + " in " + message.location, details);
   });
 
-  socket.on('resolve_entry_response', function (message) {
+  socket.on('resolve_entry_response', function(message) {
       $('#queue-entry-' + message.id).remove();
       $('#resolved').append(message.html);
   });
