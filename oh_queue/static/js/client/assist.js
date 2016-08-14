637e4c701a885e20bf7093cd41bf16df8177cd4c
Kyle Raftogianis <knrafto@gmail.com>
2016-08-13 20:51:01 -0700

    Reindent javascript

--- oh_queue/static/js/client/assist.js
+++ oh_queue/static/js/client/assist.js
@@ -3,10 +3,10 @@ $(document).ready(function(){
 
   // Bind event listeners
   $('body').on('click', '.resolve', function(event) {
-     var url = $(this).attr('data-url');
-     $.post(url, {
-       id: $(this).attr('data-id')
-     });
+    var url = $(this).attr('data-url');
+    $.post(url, {
+      id: $(this).attr('data-id')
+    });
   });
 
   // opening Socket
@@ -16,15 +16,15 @@ $(document).ready(function(){
 
   // Socket handler for adding entries
   socket.on('add_entry_response', function(message) {
-      $('#queue').append(message.html);
-      var details = {
-          body: message.name + " - " + message.assignment + message.question + " in " + message.location
-      }
-      notifyUser("OH Queue: " + message.name + " in " + message.location, details);
+    $('#queue').append(message.html);
+    var details = {
+      body: message.name + " - " + message.assignment + message.question + " in " + message.location
+    }
+    notifyUser("OH Queue: " + message.name + " in " + message.location, details);
   });
 
   socket.on('resolve_entry_response', function(message) {
-      $('#queue-entry-' + message.id).remove();
-      $('#resolved').append(message.html);
+    $('#queue-entry-' + message.id).remove();
+    $('#resolved').append(message.html);
   });
 });
--- oh_queue/static/js/client/student.js
+++ oh_queue/static/js/client/student.js
@@ -1,71 +1,71 @@
 $(document).ready(function(){
-    // Variables
-    var socket = io.connect('http://' + document.domain + ':' + location.port);
+  // Variables
+  var socket = io.connect('http://' + document.domain + ':' + location.port);
 
-    // Helper Function
-    function scrollBottom() {
-        $('html, body').animate({
-            scrollTop: $(document).height()
-        }, 600);
-    }
+  // Helper Function
+  function scrollBottom() {
+    $('html, body').animate({
+      scrollTop: $(document).height()
+    }, 600);
+  }
 
-    function toggleHelpForm() {
-        $('#help-form-container').slideToggle('medium');
-        $('#add-entry').slideToggle('medium');
-    }
+  function toggleHelpForm() {
+    $('#help-form-container').slideToggle('medium');
+    $('#add-entry').slideToggle('medium');
+  }
 
-    // Add event listeners
-    $('#add-entry').click(function() {
-        toggleHelpForm();
-        scrollBottom();
+  // Add event listeners
+  $('#add-entry').click(function() {
+    toggleHelpForm();
+    scrollBottom();
+  });
+
+  $('#cancel-form').click(function() {
+    toggleHelpForm();
+  })
+
+  var student_sid = null;
+
+  $('#help-form').submit(function(event) {
+    NProgress.start();
+    event.preventDefault();
+
+    var request = $.post('/add_entry', {
+      name: $('#name').val(),
+      sid: $('#sid').val(),
+      location: $('#location').val(),
+      assignment_type: $('#assignment_type').val(),
+      assignment: $('#assignment').val(),
+      question: $('#question').val()
     });
 
-    $('#cancel-form').click(function() {
-        toggleHelpForm();
+    request.done(function(msg) {
+      toggleHelpForm();
+      // Get permissions to notify users
+      requestNotificationPermission();
+      student_sid = $('#sid').val();
+      NProgress.done();
+      if (msg.result === 'failure') {
+        alert('Your help request could not be added. Possible reason: ' + msg.error);
+      }
     })
-
-    var student_sid = null;
-
-    $('#help-form').submit(function(event) {
-        NProgress.start();
-        event.preventDefault();
-
-        var request = $.post('/add_entry', {
-            name: $('#name').val(),
-            sid: $('#sid').val(),
-            location: $('#location').val(),
-            assignment_type: $('#assignment_type').val(),
-            assignment: $('#assignment').val(),
-            question: $('#question').val()
-        });
-
-        request.done(function(msg) {
-            toggleHelpForm();
-            // Get permissions to notify users
-            requestNotificationPermission();
-            student_sid = $('#sid').val();
-            NProgress.done();
-            if (msg.result === 'failure') {
-                alert('Your help request could not be added. Possible reason: ' + msg.error);
-            }
-        })
-        .fail(function (msg) {
-            toggleHelpForm();
-            NProgress.done();
-            // Currently, the alert doesn't work and no messages are displayed
-            alert('Your help request could not be added.');
-        });
+    .fail(function (msg) {
+      toggleHelpForm();
+      NProgress.done();
+      // Currently, the alert doesn't work and no messages are displayed
+      alert('Your help request could not be added.');
     });
+  });
 
-    socket.on('add_entry_response', function(message) {
-        $('#queue').append(message.html);
-        $('#' + message.id).slideToggle('medium');
-    });
+  socket.on('add_entry_response', function(message) {
+    $('#queue').append(message.html);
+    $('#' + message.id).slideToggle('medium');
+  });
 
-    socket.on('resolve_entry_response', function (message) {
-        if (student_sid != null && message.sid == student_sid) {
-            notifyUser("61A Queue: Your name has been called", {});
-        }
-        $('#queue-entry-' + message.id).remove();
-    });
+  socket.on('resolve_entry_response', function (message) {
+    if (student_sid != null && message.sid == student_sid) {
+      notifyUser("61A Queue: Your name has been called", {});
+    }
+    $('#queue-entry-' + message.id).remove();
+  });
 });
