b6ddaebf47e0e1c9f0e246e5f90f47fde3387f1d
Kyle Raftogianis <knrafto@gmail.com>
2016-09-03 02:19:55 -0700

    Cache assets for a year
    Webassets will version them with a hash in the query string, so they
    will be reloaded when the file changes.

--- oh_queue/__init__.py
+++ oh_queue/__init__.py
@@ -3,7 +3,7 @@ logging.basicConfig(level=logging.INFO)
 
 
 # Flask-related stuff
-from flask import Flask
+from flask import Flask, request
 from flask_socketio import SocketIO
 
 from oh_queue import assets, auth
@@ -27,6 +27,15 @@ db.init_app(app)
 auth.init_app(app)
 socketio = SocketIO(app)
 
-
 # Import views
 import oh_queue.views
+
+# Caching
+@app.after_request
+def after_request(response):
+    if request.path.startswith('/static'):
+        cache_control = 'max-age=31556926'
+    else:
+        cache_control = 'no-store'
+    response.headers.add('Cache-Control', cache_control)
+    return response
