cd40666493355630632ff660f690e159b809d0d6
Kyle Raftogianis <knrafto@gmail.com>
2016-09-03 13:42:16 -0700

    Use babel

--- oh_queue/assets.py
+++ oh_queue/assets.py
@@ -1,8 +1,13 @@
-from webassets import Environment
+import webassets
+import webassets.filter
 
-assets_env = Environment(
+assets_env = webassets.Environment(
     directory='oh_queue/static',
-    url='/static')
+    url='/static',
+)
+assets_env.config['BABEL_BIN'] = 'node_modules/babel-cli/bin/babel.js'
+
+babel = webassets.filter.get_filter('babel', presets='es2015')
 
 assets_env.register('style.css',
     'css/style.css',
@@ -11,15 +16,18 @@ assets_env.register('style.css',
 
 assets_env.register('common.js',
     'js/common.js',
+    filters=babel,
     output='public/common.js',
 )
 
 assets_env.register('index.js',
     'js/index.js',
+    filters=babel,
     output='public/index.js',
 )
 
 assets_env.register('ticket.js',
     'js/ticket.js',
+    filters=babel,
     output='public/ticket.js',
 )
