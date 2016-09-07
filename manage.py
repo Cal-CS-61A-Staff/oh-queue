f528e51a7bf6f9f4d4ce9ecddd43266209642bb2
Kyle Raftogianis <knrafto@gmail.com>
2016-09-07 01:34:22 -0700

    Use 100 students as a benchmark

--- manage.py
+++ manage.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from flask_script import Manager
 
-from oh_queue import app, socketio
+from oh_queue import app, models, socketio
 from oh_queue.models import db
 
 manager = Manager(app)
@@ -12,6 +12,23 @@ def resetdb():
     db.drop_all(app=app)
     print('Creating tables...')
     db.create_all(app=app)
+    print('Seeding...')
+    for i in range(100):
+        user = models.User(
+            name='Student McGee',
+            email='student{}@berkeley.edu'.format(i),
+            is_staff=False,
+        )
+        ticket = models.Ticket(
+            status=models.TicketStatus.pending,
+            user=user,
+            assignment='Scheme',
+            question='13',
+            location='190 Morgan Hall',
+        )
+        db.session.add(user)
+        db.session.add(ticket)
+    db.session.commit()
 
 @manager.command
 def server():
