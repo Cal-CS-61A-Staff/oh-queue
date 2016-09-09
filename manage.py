43ee94a12c72f09dbae0d763551de69434e351ad
Sumukh Sridhara <sumukh1@gmail.com>
2016-09-09 00:12:55 -0700

    add a primitive seeding script

--- manage.py
+++ manage.py
@@ -2,9 +2,25 @@
 from flask_script import Manager
 
 from oh_queue import app, socketio
-from oh_queue.models import db
+from oh_queue.models import db, Ticket, User, TicketStatus
 
 manager = Manager(app)
+
+@manager.command
+def seed():
+    print('Seeding...')
+    for i in range(5):
+        name, email = "Bear, Oski{}".format(i), "test{}@cs61a.org".format(i)
+        student = User(name=name, email=email)
+        db.session.add(student)
+        db.session.commit()
+
+        ticket = Ticket(user=student, status=TicketStatus.pending,
+                        assignment='Hog', question=5,
+                        location='Soda')
+        db.session.add(ticket)
+        db.session.commit()
+
 
 @manager.command
 def resetdb():
@@ -12,6 +28,7 @@ def resetdb():
     db.drop_all(app=app)
     print('Creating tables...')
     db.create_all(app=app)
+    seed()
 
 @manager.command
 def server():
