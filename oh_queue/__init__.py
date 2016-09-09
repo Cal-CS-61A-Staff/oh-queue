4f84ce450734af36a86f9948ec2f6f02f676a83c
Sumukh Sridhara <sumukh1@gmail.com>
2016-09-09 11:38:49 -0700

    include sentry tracking on backend and frontend

--- oh_queue/__init__.py
+++ oh_queue/__init__.py
@@ -8,10 +8,15 @@ from flask_socketio import SocketIO
 
 from oh_queue import auth
 from oh_queue.models import db, TicketStatus
+from raven.contrib.flask import Sentry
 
 # Initialize the application
 app = Flask(__name__)
 app.config.from_object('config')
+
+if not app.debug:
+    sentry = Sentry(app,
+                    dsn='https://d5dd390197e84e3ebb4779ab381610a0:ccac76da30704fa7a352a9ec3bd1708f@sentry.cs61a.org/14')
 
 app.jinja_env.globals.update({
   'TicketStatus': TicketStatus
