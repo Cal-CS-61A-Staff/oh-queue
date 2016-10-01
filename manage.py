e7fa16f288d7f66f17421a720acdf73db2ccc6b5
Kyle Raftogianis <knrafto@gmail.com>
2016-10-01 13:43:15 -0700

    Generate better seed data

--- manage.py
+++ manage.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
+import datetime
+import random
+
 from flask_script import Manager
+import names
 
 from oh_queue import app, socketio
 from oh_queue.models import db, Ticket, User, TicketStatus
@@ -10,16 +14,28 @@ manager = Manager(app)
 def seed():
     print('Seeding...')
     for i in range(20):
-        name, email = "Oski Bear {}".format(i), "test{}@cs61a.org".format(i)
+        real_name = names.get_full_name()
+        first_name, last_name = real_name.lower().split(' ')
+        email = '{0}{1}@{2}'.format(
+            random.choice([first_name, first_name[0]]),
+            random.choice([last_name, last_name[0]]),
+            random.choice(['berkeley.edu', 'gmail.com']),
+        )
         student = User.query.filter_by(email=email).one_or_none()
         if not student:
-            student = User(name=name, email=email)
+            student = User(name=real_name, email=email)
             db.session.add(student)
             db.session.commit()
 
-        ticket = Ticket(user=student, status=TicketStatus.pending,
-                        assignment='Hog', question=5,
-                        location='Soda')
+        delta = datetime.timedelta(minutes=random.randrange(0, 30))
+        ticket = Ticket(
+            user=student,
+            status=TicketStatus.pending,
+            created=datetime.datetime.utcnow() - delta,
+            assignment=random.choice(['Hog', 'Scheme']),
+            question=random.randrange(1, 6),
+            location=random.choice(['109 Morgan', '247 Cory']),
+        )
         db.session.add(ticket)
         db.session.commit()
 
