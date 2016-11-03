2aa7e3123998fc4ef4201e73fdee5699b155b090
Kevin Lin <kevinlin1@berkeley.edu>
2016-11-03 00:58:30 +0000

    Panelize and add padding margin between filter controls

--- oh_queue/static/js/components/ticket.js
+++ oh_queue/static/js/components/ticket.js
@@ -1,12 +1,12 @@
 let Ticket = ({state, ticket, myTicket, index}) => {
   return (
     <TicketLink state={state} ticket={ticket} myTicket={myTicket}>
-      <div className="hidden-xs col-sm-1 truncate">{ index + 1 }</div>
-      <div className="hidden-xs col-sm-2 truncate">{ ticketDisplayTime(ticket) }</div>
-      <div className="col-xs-3 col-sm-2 truncate">{ticket.location}</div>
-      <div className="col-xs-3 col-sm-2 truncate">{ticket.assignment}</div>
-      <div className="col-xs-2 col-sm-2 truncate">{ticket.question}</div>
-      <div className="col-xs-4 col-sm-3 truncate">{ticketStatus(state, ticket)}</div>
+      <div className="panel-body">
+        <h2>{ticket.assignment} Q{ticket.question}<br /><small>{ticket.location}</small></h2>
+      </div>
+      <div className="panel-footer">
+        <small>#{index + 1} {ticketStatus(state, ticket)} at {ticketDisplayTime(ticket)}</small>
+      </div>
     </TicketLink>
   );
 }
@@ -14,35 +14,43 @@ let Ticket = ({state, ticket, myTicket, index}) => {
 let TicketLink = ({state, ticket, myTicket, children}) => {
   if (isTicketHelper(state, ticket) || (myTicket && myTicket.id === ticket.id)) {
     return (
-      <div className="row highlight">
-        <ReactRouter.Link to={`/${ticket.id}/`} className="ticket-link">
-          {children}
-        </ReactRouter.Link>
+      <div className="col-xs-12 col-md-4">
+        <div className="panel panel-primary">
+          <ReactRouter.Link to={`/${ticket.id}/`} className="ticket-link">
+            {children}
+          </ReactRouter.Link>
+        </div>
       </div>
     );
   } else if (isStaff(state)) {  // staff
     return (
-      <div className="row">
-        <ReactRouter.Link to={`/${ticket.id}/`} className="ticket-link">
-          {children}
-        </ReactRouter.Link>
+      <div className="col-xs-12 col-md-4">
+        <div className="panel panel-default">
+          <ReactRouter.Link to={`/${ticket.id}/`} className="ticket-link">
+            {children}
+          </ReactRouter.Link>
+        </div>
       </div>
     );
     destination = `/${ticket.id}/`;
   } else if (state.currentUser) {  // student and logged in
     return (
-      <div className="row">
-        <a href="#" className="disabled-link">
-          {children}
-        </a>
+      <div className="col-xs-12 col-md-4">
+        <div className="panel panel-default">
+          <a href="#" className="disabled-link">
+            {children}
+          </a>
+        </div>
       </div>
     );
   } else {  // logged out
     return (
-      <div className="row">
-        <a href="/login" className="ticket-link">
-          {children}
-        </a>
+      <div className="col-xs-12 col-md-4">
+        <div className="panel panel-default">
+          <a href="/login" className="ticket-link">
+            {children}
+          </a>
+        </div>
       </div>
     );
   }
