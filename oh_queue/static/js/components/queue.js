7c5d9a4e3a3c64ea317c4e1c1b3e36b35795bf6e
Kevin Lin <kevinlin1@berkeley.edu>
2016-11-03 00:43:28 +0000

    Remove table header and staff hint

--- oh_queue/static/js/components/queue.js
+++ oh_queue/static/js/components/queue.js
@@ -11,21 +11,6 @@ let Queue = ({state}) => {
         <Messages messages={state.messages}/>
         {isStaff(state) && <FilterControls filter={state.filter}/>}
         <div className="queue">
-          {isStaff(state) &&
-            <div className="row">
-              <div className="col-xs-12 alert alert-info">
-                Click on a ticket to view the student's name
-              </div>
-            </div>
-          }
-          <div className="row">
-            <div className="hidden-xs col-sm-1">#</div>
-            <div className="hidden-xs col-sm-2 ">Queue Time</div>
-            <div className="col-xs-3 col-sm-2">Location</div>
-            <div className="col-xs-3 col-sm-2">Assignment</div>
-            <div className="col-xs-2 col-sm-2">Question</div>
-            <div className="col-xs-4 col-sm-3">Status</div>
-          </div>
           {tickets.length == 0 &&
             <div className="row">
               <div className="col-xs-12 text-center">
