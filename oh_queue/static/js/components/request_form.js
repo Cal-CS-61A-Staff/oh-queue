3b2313f83a58d8c3556d508e3fbc03f148b72bb8
Sumukh Sridhara <sumukh1@gmail.com>
2016-11-06 15:56:38 -0800

    Make request form extend React.Component again

--- oh_queue/static/js/components/request_form.js
+++ oh_queue/static/js/components/request_form.js
@@ -1,7 +1,6 @@
-let RequestForm = () => {
-
-  let constructor = (props) => {
-    React.Component(props);
+class RequestForm extends React.Component {
+  constructor(props) {
+    super(props);
     let onlineUrl = getParamFromURL(window.location.search, 'oh_url')
     this.state = {
       isOnline: onlineUrl ? true : false,
@@ -10,10 +9,9 @@ let RequestForm = () => {
     };
 
     this.handleLocationChange = this.handleLocationChange.bind(this)
-    this.onlineForm = this.onlineForm.bind(this)
   }
 
-  let submit = (e) => {
+  submit(e) {
     e.preventDefault();
     let formData = {};
     let hasError = false;
@@ -31,47 +29,43 @@ let RequestForm = () => {
     if (!hasError) {
       app.makeRequest('create', formData, true);
     }
-  };
-
-  let handleLocationChange = (event) => {
-    if (event.target.value === "Online") {
-      this.state.isOnline = true;
-      this.setState();
-    }
   }
 
-  let onlineForm = () => {
-    if (this.state.isOnline) {
-      return (
+  handleLocationChange(event) {
+      if (event.target.value === "Online") {
+        this.state.isOnline = true;
+        this.setState();
+      }
+    }
+
+  render() {
+    return (
+      <form id="request-form">
         <div className="form-group form-group-lg">
           <div className="input-group">
-              <input className="form-control width-100" type="url" name="url" id="url" title="Question"
-                  defaultValue={this.state.onlineUrl}
-                  data-style="btn-lg btn-default" placeholder="https://collab.cs61a.org/oh/abc" required />
+            <SelectPicker options={ASSIGNMENTS} className="selectpicker form-control width-60" data-live-search="true" data-size="8" data-width="60%" data-style="btn-lg btn-default" id="assignment" name="assignment" title="Assignment" required />
+            <input className="form-control width-40" type="number" name="question" id="question" title="Question" placeholder="Question" min="0" step="1" required />
           </div>
         </div>
-      )
-    }
-    return;
-  }
+         {this.state.isOnline &&
+            <div className="form-group form-group-lg">
+              <div className="input-group">
+                  <input className="form-control width-100" type="url" name="url" id="url" title="Question"
+                      defaultValue={this.state.onlineUrl}
+                      data-style="btn-lg btn-default" placeholder="https://collab.cs61a.org/oh/abc" required />
+              </div>
+            </div>
+          }
+        <div className="form-group form-group-lg">
+          <div className="input-group">
+            <SelectPicker options={LOCATIONS} onChange={this.handleLocationChange}  className="selectpicker form-control width-60" data-width="60%" id="location" data-style="btn-lg btn-default" name="location" title="Location" required />
+            <span className="input-group-btn width-40 pull-left" data-width="40%">
+              <button className="btn btn-lg btn-default" onClick={this.submit}>Request<span className="hidden-xs"> Help</span></button>
+            </span>
+          </div>
+        </div>
+      </form>
+      );
 
-  return (
-    <form id="request-form">
-      <div className="form-group form-group-lg">
-        <div className="input-group">
-          <SelectPicker options={ASSIGNMENTS} className="selectpicker form-control width-60" data-live-search="true" data-size="8" data-width="60%" data-style="btn-lg btn-default" id="assignment" name="assignment" title="Assignment" required />
-          <input className="form-control width-40" type="number" name="question" id="question" title="Question" placeholder="Question" min="0" step="1" required />
-        </div>
-      </div>
-      {onlineForm}
-      <div className="form-group form-group-lg">
-        <div className="input-group">
-          <SelectPicker options={LOCATIONS} onChange={handleLocationChange}  className="selectpicker form-control width-60" data-width="60%" id="location" data-width="100%" data-style="btn-lg btn-default" name="location" title="Location" required />
-          <span className="input-group-btn width-40 pull-left">
-            <button className="btn btn-lg btn-default" onClick={submit}>Request<span className="hidden-xs"> Help</span></button>
-          </span>
-        </div>
-      </div>
-    </form>
-  );
-};
+  }
+}
