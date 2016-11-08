012a1c82e3bfccc42966d3edafcee185459cafcb
Kyle Raftogianis <knrafto@gmail.com>
2016-11-07 20:01:23 -0800

    More compact queue layout (resolves #101)

--- oh_queue/static/js/components/queue.js
+++ oh_queue/static/js/components/queue.js
@@ -1,8 +1,8 @@
 let Queue = ({state}) => {
   let myTicket = getMyTicket(state);
   let tickets = getActiveTickets(state);
-  let items = tickets.map((ticket, index) =>
-    <Ticket key={ticket.id} state={state} ticket={ticket} myTicket={myTicket} index={index} />
+  let items = tickets.map((ticket) =>
+    <Ticket key={ticket.id} state={state} ticket={ticket}/>
   );
   return (
     <div>
@@ -11,15 +11,15 @@ let Queue = ({state}) => {
         <Messages messages={state.messages}/>
         {isStaff(state) && <FilterControls filter={state.filter}/>}
         {isStaff(state) && <hr />}
-        <div className="queue">
-          {tickets.length == 0 &&
-            <div className="row">
-              <div className="col-xs-12 text-center">
-                <p className="lead">There are no help requests on the queue.</p>
+        <div className="row">
+          <div className="queue center-block">
+            {tickets.length == 0 &&
+              <div className="row">
+                <div className="col-xs-12 text-center">
+                  <p className="lead">There are no help requests on the queue.</p>
+                </div>
               </div>
-            </div>
-          }
-          <div className="row">
+            }
             {tickets.length > 0 && items}
           </div>
         </div>
