416d836b4cef7769329140a79a7d032e2a30601f
Kyle Raftogianis <knrafto@gmail.com>
2016-11-08 14:02:44 -0800

    Show number of tickets in tab title (resolves #104)

--- oh_queue/static/js/components/queue.js
+++ oh_queue/static/js/components/queue.js
@@ -1,6 +1,8 @@
 let Queue = ({state}) => {
   let myTicket = getMyTicket(state);
   let showJumbotron = !isStaff(state) && !myTicket;
+  let pendingTickets = getTickets(state, 'pending');
+  let assignedTickets = getTickets(state, 'assigned');
   let selectTab = (index) => {
     state.queueTabIndex = index;
     app.refresh();
@@ -13,16 +15,17 @@ let Queue = ({state}) => {
         {isStaff(state) && <FilterControls filter={state.filter}/>}
         {isStaff(state) && <hr />}
         <div className="row">
-          <Tabs selectedIndex={state.queueTabIndex}
-            onSelect={selectTab}>
-            <TicketList
-              label="Waiting"
-              tickets={getTickets(state, 'pending')}
-              state={state} />
-            <TicketList
-              label="Assigned"
-              tickets={getTickets(state, 'assigned')}
-              state={state} />
+          <Tabs selectedIndex={state.queueTabIndex} onSelect={selectTab}>
+            <Tab label={`Waiting (${pendingTickets.length})`}>
+              <TicketList
+                tickets={pendingTickets}
+                state={state} />
+            </Tab>
+            <Tab label={`Assigned (${assignedTickets.length})`}>
+              <TicketList
+                tickets={assignedTickets}
+                state={state} />
+            </Tab>
           </Tabs>
         </div>
       </div>
@@ -30,22 +33,20 @@ let Queue = ({state}) => {
   );
 }
 
-let TicketList = ({label, state, tickets}) => {
+let TicketList = ({state, tickets}) => {
   let items = applyFilter(state.filter, tickets).map((ticket) =>
     <Ticket key={ticket.id} state={state} ticket={ticket}/>
   );
   return (
-    <Tab label={label}>
-      <div className="queue">
-        {items.length == 0 &&
-          <div className="row">
-            <div className="col-xs-12 text-center">
-              <p className="lead">There are no help requests on the queue.</p>
-            </div>
+    <div className="queue">
+      {items.length == 0 &&
+        <div className="row">
+          <div className="col-xs-12 text-center">
+            <p className="lead">There are no help requests on the queue.</p>
           </div>
-        }
-        {items}
-      </div>
-    </Tab>
+        </div>
+      }
+      {items}
+    </div>
   );
 };
