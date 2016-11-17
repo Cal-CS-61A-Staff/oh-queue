1fe384075f014b01417455630b1a6fd407d9c82c
Kevin Lin <kevinlin1@berkeley.edu>
2016-11-17 05:00:52 +0000

    Treat tablet view as equivalent to small desktop

--- oh_queue/static/js/components/filter_controls.js
+++ oh_queue/static/js/components/filter_controls.js
@@ -17,13 +17,13 @@ class Filter extends React.Component {
     let {filter} = this.props;
     return (
       <div className="row form-group form-group-lg">
-        <div className="col-xs-12 col-md-4 filter-field">
+        <div className="col-xs-12 col-sm-4 filter-field">
           <SelectPicker options={ASSIGNMENTS} emptyOption="Any Assignment" className="selectpicker form-control" data-live-search="true" data-size="8" data-width="100%" data-style="btn-lg btn-default" id="assignment" name="assignment" title="Assignment" onChange={() => this.updateFilter()}/>
         </div>
-        <div className="col-xs-12 col-md-4 filter-field">
+        <div className="col-xs-12 col-sm-4 filter-field">
           <input className="form-control" type="number" name="question" id="question" title="Question" placeholder="Question" min="0" step="1" onChange={() => this.updateFilter()}/>
         </div>
-        <div className="col-xs-12 col-md-4 filter-field">
+        <div className="col-xs-12 col-sm-4 filter-field">
           <SelectPicker options={LOCATIONS} emptyOption="Any Location" className="selectpicker form-control" id="location" data-width="100%" data-style="btn-lg btn-default" name="location" title="Location" onChange={() => this.updateFilter()}/>
         </div>
         <hr></hr>
