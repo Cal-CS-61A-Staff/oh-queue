8ad036affe6030cadf28a37b0538e77b86459b21
Khalil Sarwari <khalil.r.sarwari@gmail.com>
2017-09-03 01:59:51 -0700

    cleaned, legitimatized interval, style

--- oh_queue/static/js/components/presence_indicator.js
+++ oh_queue/static/js/components/presence_indicator.js
@@ -1,4 +1,5 @@
-let PresenceIndicator = ({presence, state}) => {
+let PresenceIndicator = ({state}) => {
+  let presence = state.presence
   let numStudentsOnline = presence && presence.students ? presence.students : 0
   let numStaffOnline = presence && presence.staff ? presence.staff : 0
   let color = numStaffOnline ? 'success' : 'warning'
@@ -18,7 +19,7 @@ let PresenceIndicator = ({presence, state}) => {
 
   let message = studentMessage + " and " + staffMessage + " currently online."
   
-  // average time to finish helping a single student
+  // PARAM 1: average time to finish helping a single student 
   var avgHelpTime = 10
 
   // how many assistants are unoccupied
@@ -27,58 +28,59 @@ let PresenceIndicator = ({presence, state}) => {
   // how many students will have to wait until an assistant is free
   var stillNeedHelp = Math.max(0, pendingTickets.length - availableAssistants)
 
-  // expecting 10 minutes per person who still needs help
-  var estWaitTime = Math.floor(avgHelpTime * stillNeedHelp)
-
-  // construct boundaries around this time
-  // interval generally becomes smaller (sample mean approaches true mean) as more assistants available
-  var estWaitTimeMin = Math.max(0, Math.floor(estWaitTime - getRandomArbitrary(1, (availableAssistants + 10)/(availableAssistants + 1))))
-  var estWaitTimeMax = Math.ceil(estWaitTime + getRandomArbitrary(1, (availableAssistants + 10)/(availableAssistants + 1)))
-
-  // colors for the time
-  if (estWaitTime <= 5) {
-      var col ="#009900"  
-  } else if (estWaitTime < 10) {
-      var col ="#739900"
-  } else if (estWaitTime < 25) {
-      var col ="#cc5200"
-  } else {
-      var col ="#ff0000"
-  }
-
-  var timeRange = estWaitTimeMin + " - " + estWaitTimeMax
-
   // catch if there actually are no assistants available
   if (numStaffOnline == 0) {
     var timeRange = "Unknown"
     var col = "#646468"
+  } else {
+    // expecting 10 minutes per person who still needs help, scale down by number of staff
+    var estWaitTime = Math.ceil(avgHelpTime * stillNeedHelp / numStaffOnline)
+    
+    // PARAM 2: max width measured from actual est wait time to upper bound.
+    var maxWidthConstant = 20
+    // interval generally becomes smaller (sample mean approaches true mean) as more assistants available
+    var intervalConstant = Math.ceil((availableAssistants + maxWidthConstant)/(availableAssistants + 1))
+
+    var estWaitTimeMin = Math.max(0, estWaitTime - intervalConstant)
+    var estWaitTimeMax = estWaitTime + intervalConstant
+
+    // colors for the time
+    if (estWaitTime <= 5) {
+      var col ="#009900"  
+    } else if (estWaitTime < 10) {
+      var col ="#739900"
+    } else if (estWaitTime < 25) {
+      var col ="#cc5200"
+    } else {
+      var col ="#ff0000"
+    }   
+
+    var timeRange = estWaitTimeMin + " - " + estWaitTimeMax
   }
 
 
   return (
     <div className="col-xs-12">
 
-    <div className={`alert alert-${color} alert-dismissable fade in`} role="alert">
-      <button type="button" className="close" aria-label="Close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
+      <div className={`alert alert-${color} alert-dismissable fade in`} role="alert">
+        <button type="button" className="close" aria-label="Close" data-dismiss="alert">
+            <span aria-hidden="true">&times;</span>
         </button>
-      <div> <font size="6">Estimated wait time: <font color={col}><strong>{timeRange}</strong></font> minutes </font></div>
-      <p> <br></br> To help reduce wait time: <br></br></p>
-      <p> &#8594; Plan ahead what questions you want to ask (we will limit time spent to <strong> 10 </strong> minutes per person) </p>
-      <p> &#8594; When applicable, be prepared to explain your reasoning, attempts, and current approach</p>
-      <p> &#8594; Check out other resources, including <a href="http://www.piazza.com">piazza</a>, to make sure your question wasn&apos;t already answered</p>
+        <h1><font size="6">Estimated wait time: <font color={col}><strong>{timeRange}</strong></font> minutes </font></h1>
+        <p>To help reduce wait time:</p>
+        <ul className='wait-suggestions'>
+          <li>Plan ahead what questions you want to ask (we will limit time spent to <strong> 10 </strong> minutes per person)</li>
+          <li>When applicable, be prepared to explain your reasoning, attempts, and current approach</li>
+          <li>Check out other resources, including <a href="http://www.piazza.com">piazza</a>, to make sure your question wasn&apos;t already answered</li>
+        </ul>
       </div>
 
       <div className={`alert alert-${color} alert-dismissable fade in`} role="alert">
-      {message}
-      <button type="button" className="close" aria-label="Close" data-dismiss="alert">
-          <span aria-hidden="true">&times;</span>
+        {message}
+        <button type="button" className="close" aria-label="Close" data-dismiss="alert">
+            <span aria-hidden="true">&times;</span>
         </button>
       </div>
     </div>
   );
-}
-
-function getRandomArbitrary(min, max) {
-  return Math.random() * (max - min) + min;
 }
