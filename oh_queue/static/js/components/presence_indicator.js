5225f7b9b4a5a21a622bd4a3e245240f6614c3d5
Eric Pai <ericpai@berkeley.edu>
2017-11-14 16:30:11 -0800

    add message about not handling extra credit

--- oh_queue/static/js/components/presence_indicator.js
+++ oh_queue/static/js/components/presence_indicator.js
@@ -18,8 +18,8 @@ let PresenceIndicator = ({state}) => {
   }
 
   let message = studentMessage + " and " + staffMessage + " currently online."
-  
-  // PARAM 1: average time to finish helping a single student 
+
+  // PARAM 1: average time to finish helping a single student
   var avgHelpTime = 10
 
   // how many assistants are unoccupied
@@ -35,7 +35,7 @@ let PresenceIndicator = ({state}) => {
   } else {
     // expecting 10 minutes per person who still needs help, scale down by number of staff
     var estWaitTime = Math.ceil(avgHelpTime * stillNeedHelp / numStaffOnline)
-    
+
     // PARAM 2: max width measured from actual est wait time to upper bound.
     var maxWidthConstant = 20
     // interval generally becomes smaller (sample mean approaches true mean) as more assistants available
@@ -46,14 +46,14 @@ let PresenceIndicator = ({state}) => {
 
     // colors for the time
     if (estWaitTime <= 5) {
-      var col ="#009900"  
+      var col ="#009900"
     } else if (estWaitTime < 10) {
       var col ="#739900"
     } else if (estWaitTime < 25) {
       var col ="#cc5200"
     } else {
       var col ="#ff0000"
-    }   
+    }
 
     var timeRange = estWaitTimeMin + " - " + estWaitTimeMax
   }
@@ -72,6 +72,7 @@ let PresenceIndicator = ({state}) => {
           <li>Plan ahead what questions you want to ask (we will limit time spent to <strong> 10 </strong> minutes per person)</li>
           <li>When applicable, be prepared to explain your reasoning, attempts, and current approach</li>
           <li>Check out other resources, including <a href="http://www.piazza.com">piazza</a>, to make sure your question wasn&apos;t already answered</li>
+          <li>We will <strong>not</strong> be helping out with extra credit unless there is no-one else on the queue</li>
         </ul>
       </div>
 
