ef1bdf707379ede458fc5af5141c426edd83a6ec
Ja (Thanakul) Wattanawong <ja@jwattanawong.com>
2019-02-13 17:22:16 -0800

    removed a few testing changes and hide for staff

--- oh_queue/static/js/components/update_location_box.js
+++ oh_queue/static/js/components/update_location_box.js
@@ -19,19 +19,25 @@ class UpdateLocationBox extends React.Component {
 
     render() {
         let {state, ticket} = this.props;
-        return(
-            <div>
-                <h4> If you have moved, please update your new location here:</h4>
-                <div className="form-group form-group-lg">
-                    <div className="input-group">
-                        <SelectPicker onChange={this.handleChange} options={LOCATIONS} className="selectpicker form-control" id="location" data-width="80%" data-style="btn-lg btn-default" name="location" title="Location"/>
-                            <div className="input-group-btn form-right pull-left">
-                                <button className="btn btn-lg btn-default" onClick={this.submit}>Update</button>
-                            </div>
+        let staff = isStaff(state);
+        if(staff) {
+            return(null)
+        } else {
+            return(
+                <div>
+                    <h4> If you have moved, please update your new location here:</h4>
+                    <div className="form-group form-group-lg">
+                        <div className="input-group">
+                            <SelectPicker onChange={this.handleChange} options={LOCATIONS} className="selectpicker form-control" id="location" data-width="80%" data-style="btn-lg btn-default" name="location" title="Location"/>
+                                <div className="input-group-btn form-right pull-left">
+                                    <button className="btn btn-lg btn-default" onClick={this.submit}>Update</button>
+                                </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        );
+            );
+        }
+        
     }
 
 }
