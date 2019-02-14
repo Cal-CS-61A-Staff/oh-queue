683d7544e2160501815f0b94bcdd2f459f7b9bfc
Ja (Thanakul) Wattanawong <ja@jwattanawong.com>
2019-02-13 21:53:08 -0800

    check that id's match before changing

--- oh_queue/views.py
+++ oh_queue/views.py
@@ -176,6 +176,9 @@ def create(form):
 def update_location(location):
     ticket_id, new_location = location['id'], location['new_location']
     ticket = Ticket.query.filter(Ticket.id == ticket_id).first()
+    if not (current_user.is_staff or ticket.user.id == current_user.id):
+            return socket_unauthorized()
+            
     ticket.location = new_location
     emit_event(ticket, TicketEventType.update_location)
 
