8c25d8fdcf8117856e9a324efad3d06ec69a4ed5
rahularya <rahularya@berkeley.edu>
2020-03-18 01:01:10 +0800

    fix: added confirmation dialogs to appointment commands

--- oh_queue/static/js/components/appointment_buttons.js
+++ oh_queue/static/js/components/appointment_buttons.js
@@ -1,6 +1,24 @@
 function AppointmentButtons() {
     const action = action => () => {
-        app.makeRequest("bulk_appointment_action", action);
+        Swal.fire({
+          title: 'Are you sure?',
+          text: "You won't be able to revert this action!",
+          icon: 'warning',
+          showCancelButton: true,
+          confirmButtonColor: '#3085d6',
+          cancelButtonColor: '#d33',
+          confirmButtonText: 'Yes, do it!'
+        }).then((result) => {
+          if (result.value) {
+            app.makeRequest("bulk_appointment_action", action, () => {
+                Swal.fire(
+                  'Success!',
+                  'Your action has been performed.',
+                  'success'
+                )
+            });
+          }
+        });
     };
 
     const { Link } = ReactRouterDOM;
@@ -11,13 +29,13 @@ function AppointmentButtons() {
                 Add Appointments
             </Link>
             <button className="btn btn-warning" onClick={action("open_all_assigned")}>
-                Open all assigned appointments
+                Activate all assigned appointments
             </button>
             <button className="btn btn-primary" onClick={action("resolve_all_past")}>
                 Resolve all past appointments
             </button>
             <button className="btn btn-danger" onClick={action("remove_all_unassigned")}>
-                Remove all unassigned appointments
+                Delete all unassigned appointments
             </button>
         </div>
     );
