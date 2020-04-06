028feef27ad3dd150e054b5aed63b92aa0e52736
Rahul Arya <rahularya@berkeley.edu>
2020-04-06 14:15:04 +0800

    feat: refactored admin panel (#206)

--- oh_queue/static/js/components/config_linked_toggle.js
+++ oh_queue/static/js/components/config_linked_toggle.js
@@ -2,14 +2,14 @@ function ConfigLinkedToggle({ config, configKey, offText = "No", onText = "Yes" 
     if (!app || Object.keys(config).length === 0) {
         return null;
     }
-    const handleChange = (value) => {
-        const configValue = value ? "true" : "false";
-        app.makeRequest(`update_config`, {
-            key: configKey,
-            value: configValue,
-        });
-    };
-
-    return <FancyToggle checked={config[configKey] === "true"} offText={offText} onText={onText}
-                        onChange={handleChange}/>;
+    return (
+        <ConfigLinked configKey={configKey} config={config} render={({ onSubmit, value }) => (
+            <FancyToggle
+                checked={value === "true"}
+                offText={offText}
+                onText={onText}
+                onChange={x => onSubmit(x ? "true" : "false")}
+            />
+        )}/>
+    );
 }
