4aabc5449aa76dc7a2542cdfc23c822f550d9b10
rahularya <rahularya@berkeley.edu>
2020-08-28 04:43:39 -0700

    hotfix for new assignments admin UI

--- oh_queue/static/js/components/admin_items_boolean_field.js
+++ oh_queue/static/js/components/admin_items_boolean_field.js
@@ -1,6 +1,6 @@
 function AdminItemsBooleanField({ itemType, propType, item, onText="Yes", offText="No" }) {
     const handleChange = (value) => {
-        app.makeRequest("update_location", { id: item.id, [propType]: value });
+        app.makeRequest(`update_${itemType}`, { id: item.id, [propType]: value });
     }
 
     return <FancyToggle onText={onText} offText={offText} checked={item[propType]} onChange={handleChange} />;
